3b34e7b4555bc355b12743e0e71dd064abd95ea6
Mark Nyon <mark@grandkru.com>
2017-04-21 11:47:37 -0400

    Update update_cloud9_template.sql
    Removing the postgres prompt from individual SQL commands.

--- cloud9/update_cloud9_template.sql
+++ cloud9/update_cloud9_template.sql
@@ -3,20 +3,16 @@
     Long term goal is to make this an automated script, but for now this is a
     listing of the SQL commands needed to install a new template.
     Mark Nyon 03/19/2017
+   
+    Shell command to run first
+    sudo sudo -u postgres psql
+
 */ 
 
-sudo sudo -u postgres psql
-
-postgres# UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
-
-postgres# DROP DATABASE template1;
-
-postgres# CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';
-
-postgres# UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
-
-postgres# \c template1
-
-postgres# VACUUM FREEZE;
-
-postgres# \q 
+UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
+DROP DATABASE template1;
+CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE';
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
+\c template1
+VACUUM FREEZE;
+\q 
